ecce9376b9d43d496dda82fecb7cd425ace1588a
frknkrc44 <krc440002@gmail.com>
2021-10-19 03:56:27 +0300

    add fallback for magisk script

--- create_zip.sh
+++ create_zip.sh
@@ -24,6 +24,29 @@ mkdir -p $APP_DIR
 cp app/build/outputs/apk/$BUILD_TYPE/app-$BUILD_TYPE.apk $APP_DIR/BatStats.apk
 mkdir -p $UPDATER_DIR
 curl -L https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh > $UPDATER_DIR/update-binary
+if [ $? -ne 0 ]
+then
+  echo "Download failed, using fallback script"
+  cat << EOF > $UPDATER_DIR/update-binary
+#!/sbin/sh
+umask 022
+require_new_magisk() {
+  echo "*******************************"
+  echo " Please install Magisk v20.4+! "
+  echo "*******************************"
+  exit 1
+}
+OUTFD=$2
+ZIPFILE=$3
+mount /data 2>/dev/null
+[ -f /data/adb/magisk/util_functions.sh ] || require_new_magisk
+. /data/adb/magisk/util_functions.sh
+[ $MAGISK_VER_CODE -lt 20400 ] && require_new_magisk
+install_module
+exit 0
+EOF
+fi
+
 echo "#MAGISK" > $UPDATER_DIR/updater-script
 cat << EOF > $BUILD_DIR/module.prop
 id=batstats12
